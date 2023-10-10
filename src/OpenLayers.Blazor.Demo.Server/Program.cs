dceef4e64ae6ffd9dc4d6f6b35bce6ceda71a877
lolochristen <lolochristen@users.noreply.github.com>
2023-10-10 13:50:46 +0200

    shared demo pages

--- src/OpenLayers.Blazor.Demo.Server/Program.cs
+++ src/OpenLayers.Blazor.Demo.Server/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddHttpClient();
 
 var app = builder.Build();
 

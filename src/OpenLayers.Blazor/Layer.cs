465253b628a60431f7066865198aab3a4dfef71e
lolochristen <lolochristen@users.noreply.github.com>
2023-10-24 21:35:13 +0200

    Fixed WMS param hanlding Added German GDZ Demo

--- src/OpenLayers.Blazor/Layer.cs
+++ src/OpenLayers.Blazor/Layer.cs
@@ -175,11 +175,7 @@ public class Layer : ComponentBase
     public string? Format
     {
         get => _internalLayer.Source.Format;
-        set
-        {
-            _internalLayer.Source.Format = value;
-            _internalLayer.Source.Params["format"] = value;
-        }
+        set => _internalLayer.Source.Format = value;
     }
 
     [Parameter]
@@ -199,15 +195,22 @@ public class Layer : ComponentBase
     [Parameter]
     public string? Layers
     {
-        get => _internalLayer.Source.Params.ContainsKey("layers") ? _internalLayer.Source.Params["layers"].ToString() : null;
-        set => _internalLayer.Source.Params["layers"] = value;
+        get => _internalLayer.Source.Params.ContainsKey("LAYERS") ? _internalLayer.Source.Params["LAYERS"].ToString() : null;
+        set => _internalLayer.Source.Params["LAYERS"] = value;
+    }
+
+    [Parameter]
+    public string? Styles
+    {
+        get => _internalLayer.Source.Params.ContainsKey("STYLES") ? _internalLayer.Source.Params["STYLES"].ToString() : null;
+        set => _internalLayer.Source.Params["STYLES"] = value;
     }
 
     [Parameter]
     public string? Title
     {
-        get => _internalLayer.Properties.ContainsKey("title") ? _internalLayer.Properties["title"].ToString() : null;
-        set => _internalLayer.Properties["title"] = value;
+        get => _internalLayer.Properties.ContainsKey("TITLE") ? _internalLayer.Properties["TITLE"].ToString() : null;
+        set => _internalLayer.Properties["TITLE"] = value;
     }
 
     protected override void OnInitialized()

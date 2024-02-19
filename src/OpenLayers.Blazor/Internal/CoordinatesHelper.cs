d061446249aa95001fe07d338a9c47c840fb851f
lolochristen <mailto:lolochristen@users.noreply.github.com>
2024-02-19 21:53:31 +0100

    Clean up CoordinatesHelper

--- src/OpenLayers.Blazor/Internal/CoordinatesHelper.cs
+++ src/OpenLayers.Blazor/Internal/CoordinatesHelper.cs
@@ -5,64 +5,6 @@ namespace OpenLayers.Blazor.Internal;
 
 internal static class CoordinatesHelper
 {
-    public static Coordinate? GetPoint(dynamic coordinates)
-    {
-        if (coordinates is double[] c1)
-            return new Coordinate(c1);
-        if (coordinates is IEnumerable<double[]> c2)
-            return new Coordinate(c2.First());
-        if (coordinates is IEnumerable<IEnumerable<double[]>> c3)
-            return new Coordinate(c3.First().First());
-        return null;
-    }
-
-    public static double[] SetPoint(Coordinate coordinate)
-    {
-        return coordinate.Value;
-    }
-
-    public static bool IsSingleCoordinate(dynamic coordinates)
-    {
-        return !IsMultiCoordinate(coordinates) && (coordinates is double[] || ((coordinates is IEnumerable<double[]> c1) && c1.Count() == 1));
-    }
-
-    public static bool IsMultiCoordinate(dynamic coordinates)
-    {
-        return coordinates is IEnumerable<IEnumerable<double[]>>;
-    }
-
-    public static IEnumerable<Coordinate>? GetCoordinates(dynamic coordinates)
-    {
-        if (coordinates is double[] c1)
-            return new[] { new Coordinate(c1) };
-        if (coordinates is IEnumerable<double[]> c2)
-            return c2.Select(p => new Coordinate(p));
-        if (coordinates is IEnumerable<IEnumerable<double[]>> c3)
-            return c3.First().Select(p => new Coordinate(p));
-        return null;
-    }
-
-    public static double[][] SetCoordinates(IEnumerable<Coordinate> coordinates)
-    {
-        return coordinates.Select(p => p.Value).ToArray();
-    }
-
-    public static IEnumerable<IEnumerable<Coordinate>>? GetMultiCoordinates(dynamic coordinates)
-    {
-        if (coordinates is double[] c1)
-            return new[] { new[] { new Coordinate(c1) } };
-        if (coordinates is IEnumerable<double[]> c2)
-            return new[] { c2.Select(p => new Coordinate(p)) };
-        if (coordinates is IEnumerable<IEnumerable<double[]>> c3)
-            return c3.Select(p => p.Select(p2 => new Coordinate(p2)));
-        return null;
-    }
-
-    public static double[][][] SetMultiCoordinates(IEnumerable<IEnumerable<Coordinate>> coordinates)
-    {
-        return coordinates.Select(p => p.Select(p => p.Value).ToArray()).ToArray();
-    }
-
     public static Coordinates? DeserializeCoordinates(JsonElement element)
     {
         int level = 0;

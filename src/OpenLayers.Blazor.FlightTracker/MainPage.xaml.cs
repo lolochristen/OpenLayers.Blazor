833c8ba3fbc1c991dba227999ed3f86f890db24e
lolochristen <lolochristen@users.noreply.github.com>
2024-08-06 22:06:26 +0200

    Upgrades. New MAUI example app FlightTracker.

---
+++ src/OpenLayers.Blazor.FlightTracker/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace OpenLayers.Blazor.FlightTracker
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/OpenLayers.Blazor.FlightTracker/MauiProgram.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace OpenLayers.Blazor.FlightTracker
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            builder.Services.AddHttpClient();
+            builder.Services.AddMauiBlazorWebView();
+
+#if DEBUG
+    		builder.Services.AddBlazorWebViewDeveloperTools();
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
